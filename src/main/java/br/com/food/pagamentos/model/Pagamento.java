a14a9fda1343e4f46e2838e9d93f627dee11a4e9
Wenderson Ferreira <129904510+wenderson-fe@users.noreply.github.com>
2025-12-11 20:46:56 -0300

    refactor(pagamento): substitui getters e setters manuais por anotações do Lombok

--- src/main/java/br/com/food/pagamentos/model/Pagamento.java
+++ src/main/java/br/com/food/pagamentos/model/Pagamento.java
@@ -1,11 +1,17 @@
 package br.com.food.pagamentos.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "pagamentos")
+@Setter
+@Getter
+@NoArgsConstructor
 public class Pagamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,79 +26,4 @@ public class Pagamento {
     private Status status;
     private Long pedidoId;
     private Long formaDePagamentoId;
-
-    public Pagamento() {
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public void setExpiracao(String expiracao) {
-        this.expiracao = expiracao;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public void setPedidoId(Long pedidoId) {
-        this.pedidoId = pedidoId;
-    }
-
-    public void setFormaDePagamentoId(Long formaDePagamentoId) {
-        this.formaDePagamentoId = formaDePagamentoId;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public String getExpiracao() {
-        return expiracao;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public Long getFormaDePagamentoId() {
-        return formaDePagamentoId;
-    }
-
-    public Long getPedidoId() {
-        return pedidoId;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
